7104f681c9aae8252c859b4e4fbf78e8de9a8fa5
Tectin <noah.nachtigall@gmail.com>
2024-04-28 21:23:55 +0200

    added ability to draw multiple lines and extracted some code

--- src/windows/plot.rs
+++ src/windows/plot.rs
@@ -19,6 +19,7 @@ pub struct PlotWindow {
     id: String,
     request_close: bool,
     line_info: Option<LineInfo>,
+    lines: Vec<LineInfo>,
 }
 
 impl PlotWindow {
@@ -35,6 +36,7 @@ impl PlotWindow {
             id,
             request_close: false,
             line_info: None,
+            lines: vec![],
         }
     }
 
@@ -45,6 +47,129 @@ impl PlotWindow {
     pub fn is_request_close(&self) -> bool {
         self.request_close
     }
+
+    fn new_plot_window(&mut self) -> Plot {
+        let plot = Plot::new(format!("{}", &self.symbol))
+            .x_axis_formatter(|gridmark: egui_plot::GridMark, _, _| {
+                let date = chrono::DateTime::from_timestamp(gridmark.value as i64, 0).unwrap();
+
+                date.format("%Y-%m-%d").to_string()
+            })
+            .label_formatter(|_, plot_point| {
+                format!(
+                    "Date: {}\nValue: {}",
+                    chrono::DateTime::from_timestamp(plot_point.x as i64, 0)
+                        .unwrap()
+                        .format("%Y-%m-%d")
+                        .to_string(),
+                    plot_point.y
+                )
+            })
+            .coordinates_formatter(
+                Corner::LeftTop,
+                CoordinatesFormatter::new(|plot_point, _| {
+                    format!(
+                        "Date: {}\nValue: {}",
+                        chrono::DateTime::from_timestamp(plot_point.x as i64, 0)
+                            .unwrap()
+                            .format("%Y-%m-%d")
+                            .to_string(),
+                        plot_point.y
+                    )
+                }),
+            );
+        plot
+    }
+
+    fn plot_show(
+        &mut self,
+        plot: Plot,
+        ui: &mut egui::Ui,
+        transform: &mut Option<egui_plot::PlotTransform>,
+    ) -> egui_plot::PlotResponse<()> {
+        let plot_response = plot.show(ui, |ui| {
+            *transform = Some(*ui.transform());
+
+            let boxplot = BoxPlot::new(
+                self.quotes
+                    .iter()
+                    .map(|quote| {
+                        let lower_whisker = quote.low;
+                        let lower_quartile = quote.open.min(quote.close);
+                        let median = (quote.open + quote.close) / 2.0;
+                        let upper_quartile = quote.open.max(quote.close);
+                        let upper_whisker = quote.high;
+
+                        let color = if quote.open < quote.close {
+                            egui::Color32::from_rgb(0, 255, 0)
+                        } else {
+                            egui::Color32::from_rgb(255, 0, 0)
+                        };
+
+                        BoxElem::new(
+                            quote.timestamp as f64,
+                            BoxSpread::new(
+                                lower_whisker,
+                                lower_quartile,
+                                median,
+                                upper_quartile,
+                                upper_whisker,
+                            ),
+                        )
+                        .whisker_width(0.0)
+                        .box_width(40000.0)
+                        .fill(color)
+                        .stroke(egui::Stroke::new(1.0, color))
+                    })
+                    .collect::<Vec<_>>(),
+            )
+            .element_formatter(Box::new(|elem, _| {
+                let is_red = elem.fill == egui::Color32::from_rgb(255, 0, 0);
+
+                let (open, close) = if is_red {
+                    (elem.spread.quartile1, elem.spread.quartile3)
+                } else {
+                    (elem.spread.quartile3, elem.spread.quartile1)
+                };
+
+                let x = elem.argument as i64;
+
+                let x = chrono::DateTime::from_timestamp(x, 0)
+                    .unwrap()
+                    .format("%Y-%m-%d")
+                    .to_string();
+
+                let low = elem.spread.lower_whisker;
+                let high = elem.spread.upper_whisker;
+
+                format!(
+                    "Date: {}\n\nOpen: {:.2}\nClose: {:.2}\nHigh: {:.2}\nLow: {:.2}",
+                    x, open, close, high, low
+                )
+            }));
+
+            ui.box_plot(boxplot);
+
+            for line in self.lines.iter() {
+                ui.line(Line::new(PlotPoints::new(
+                    [line.start, line.end]
+                        .iter()
+                        .map(|pos| [pos[0], pos[1]])
+                        .collect::<Vec<_>>(),
+                )));
+            }
+
+            if let Some(line_info) = &self.line_info {
+                ui.line(Line::new(PlotPoints::new(
+                    [line_info.start, line_info.end]
+                        .iter()
+                        .map(|pos| [pos[0], pos[1]])
+                        .collect::<Vec<_>>(),
+                )))
+            }
+        });
+        plot_response
+    }
 }
 
 impl ViewWindow for PlotWindow {
@@ -53,111 +178,11 @@ impl ViewWindow for PlotWindow {
             .id(self.id.clone().into())
             .title_bar(true)
             .show(ui.ctx(), |ui| {
-                let plot = Plot::new(format!("{}", &self.symbol))
-                    .x_axis_formatter(|gridmark: egui_plot::GridMark, _, _| {
-                        let date =
-                            chrono::DateTime::from_timestamp(gridmark.value as i64, 0).unwrap();
-
-                        date.format("%Y-%m-%d").to_string()
-                    })
-                    .label_formatter(|_, plot_point| {
-                        format!(
-                            "Date: {}\nValue: {}",
-                            chrono::DateTime::from_timestamp(plot_point.x as i64, 0)
-                                .unwrap()
-                                .format("%Y-%m-%d")
-                                .to_string(),
-                            plot_point.y
-                        )
-                    })
-                    .coordinates_formatter(
-                        Corner::LeftTop,
-                        CoordinatesFormatter::new(|plot_point, _| {
-                            format!(
-                                "Date: {}\nValue: {}",
-                                chrono::DateTime::from_timestamp(plot_point.x as i64, 0)
-                                    .unwrap()
-                                    .format("%Y-%m-%d")
-                                    .to_string(),
-                                plot_point.y
-                            )
-                        }),
-                    );
+                let plot = self.new_plot_window();
 
                 let mut transform = None;
 
-                let plot_response = plot.show(ui, |ui| {
-                    transform = Some(*ui.transform());
-
-                    let boxplot = BoxPlot::new(
-                        self.quotes
-                            .iter()
-                            .map(|quote| {
-                                let lower_whisker = quote.low;
-                                let lower_quartile = quote.open.min(quote.close);
-                                let median = (quote.open + quote.close) / 2.0;
-                                let upper_quartile = quote.open.max(quote.close);
-                                let upper_whisker = quote.high;
-
-                                let color = if quote.open < quote.close {
-                                    egui::Color32::from_rgb(0, 255, 0)
-                                } else {
-                                    egui::Color32::from_rgb(255, 0, 0)
-                                };
-
-                                BoxElem::new(
-                                    quote.timestamp as f64,
-                                    BoxSpread::new(
-                                        lower_whisker,
-                                        lower_quartile,
-                                        median,
-                                        upper_quartile,
-                                        upper_whisker,
-                                    ),
-                                )
-                                .whisker_width(0.0)
-                                .box_width(40000.0)
-                                .fill(color)
-                                .stroke(egui::Stroke::new(1.0, color))
-                            })
-                            .collect::<Vec<_>>(),
-                    )
-                    .element_formatter(Box::new(|elem, _| {
-                        let is_red = elem.fill == egui::Color32::from_rgb(255, 0, 0);
-
-                        let (open, close) = if is_red {
-                            (elem.spread.quartile1, elem.spread.quartile3)
-                        } else {
-                            (elem.spread.quartile3, elem.spread.quartile1)
-                        };
-
-                        let x = elem.argument as i64;
-
-                        let x = chrono::DateTime::from_timestamp(x, 0)
-                            .unwrap()
-                            .format("%Y-%m-%d")
-                            .to_string();
-
-                        let low = elem.spread.lower_whisker;
-                        let high = elem.spread.upper_whisker;
-
-                        format!(
-                            "Date: {}\n\nOpen: {:.2}\nClose: {:.2}\nHigh: {:.2}\nLow: {:.2}",
-                            x, open, close, high, low
-                        )
-                    }));
-
-                    ui.box_plot(boxplot);
-
-                    if let Some(line_info) = &self.line_info {
-                        ui.line(Line::new(PlotPoints::new(
-                            [line_info.start, line_info.end]
-                                .iter()
-                                .map(|pos| [pos[0], pos[1]])
-                                .collect::<Vec<_>>(),
-                        )))
-                    }
-                });
+                let plot_response = self.plot_show(plot, ui, &mut transform);
 
                 let plot_rect = plot_response.response.rect;
 
@@ -197,6 +222,10 @@ impl ViewWindow for PlotWindow {
                                 line_info.is_fixed = true;
                             }
                         }
+                    }
+
+                    if self.line_info.as_ref().unwrap().is_fixed {
+                        self.lines.push(self.line_info.take().unwrap());
                     }
                 }
 
